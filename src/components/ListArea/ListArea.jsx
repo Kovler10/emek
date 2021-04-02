b632c38181a5535ddf9e332667368c4f7e41b9f9
Ori Cohen <cohenori242@gmail.com>
2021-04-02 12:40:33 +0300

    Added delim and wrapper option

--- src/components/ListArea/ListArea.jsx
+++ src/components/ListArea/ListArea.jsx
@@ -4,22 +4,27 @@ import { ListContext } from "../../context/ListContext.jsx";
 
 const ListArea = ({ title, listIndex = -1, readonly = false }) => {
   const [value, setValue] = useState();
+  const [delim, setDelim] = useState("\n");
+  const [wrapper, setWrapper] = useState("");
   const { listA, listB, setListByIndex, getListByIndex } = useContext(
     ListContext
   );
 
   useEffect(() => {
     if (readonly) {
-      const displayList = getListByIndex(listIndex).join("\n");
+      const displayList = getListByIndex(listIndex)
+        .map((value) => wrapper + value + wrapper)
+        .join(delim);
       setValue(displayList);
     }
-  }, [listA, listB, getListByIndex, listIndex, readonly]);
+  }, [listA, listB, getListByIndex, listIndex, readonly, delim, wrapper]);
 
   const onTextInput = (event) => {
     setValue(event.target.value);
     const valueList = event.target.value.split("\n");
     setListByIndex(valueList, listIndex);
   };
+
   return (
     <div>
       <div className="input-box-title">{title}</div>
@@ -29,6 +34,24 @@ const ListArea = ({ title, listIndex = -1, readonly = false }) => {
         value={value}
         onChangeCapture={onTextInput}
       ></textarea>
+      {readonly && (
+        <div className="input-box-controls">
+          <label className="input-label">Delimeter: </label>
+          <input
+            className="input-text"
+            type="text"
+            value={delim}
+            onChange={(e) => setDelim(e.target.value ? e.target.value : "\n")}
+          />
+          <label className="input-label">Wrapper: </label>
+          <input
+            className="input-text"
+            type="text"
+            value={wrapper}
+            onChange={(e) => setWrapper(e.target.value)}
+          />
+        </div>
+      )}
     </div>
   );
 };
