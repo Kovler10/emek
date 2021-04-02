3a15aa2ab4136922ab244d8a52c44c45d223ee35
Ori Cohen <cohenori242@gmail.com>
2021-04-02 14:51:00 +0300

    Moved to context

--- src/components/ListFormatControls/ListFormatControls.jsx
+++ src/components/ListFormatControls/ListFormatControls.jsx
@@ -1,27 +1,26 @@
 import { useListContext } from "../../context/ListContext";
-
+import "./ListFormatControls.css";
 const ListFormatControls = () => {
+  const { delimiter, setDelimiter, wrapper, setWrapper } = useListContext();
 
-    const {delimiter, setDelimiter, wrapper, setWrapper} = useListContext()
-
-    return (
-        <div className="input-box-controls">
-          <label className="input-label">Delimeter: </label>
-          <input
-            className="input-text"
-            type="text"
-            value={delimiter}
-            onChange={(e) => setDelimiter(e.target.value ? e.target.value : "\n")}
-          />
-          <label className="input-label">Wrapper: </label>
-          <input
-            className="input-text"
-            type="text"
-            value={wrapper}
-            onChange={(e) => setWrapper(e.target.value)}
-          />
-        </div>
-    )
-}
+  return (
+    <div className="input-box-controls">
+      <label className="input-label">Wrapper: </label>
+      <input
+        className="input-text"
+        type="text"
+        value={wrapper}
+        onChange={(e) => setWrapper(e.target.value)}
+      />
+      <label className="input-label">Delimeter: </label>
+      <input
+        className="input-text"
+        type="text"
+        value={delimiter}
+        onChange={(e) => setDelimiter(e.target.value ? e.target.value : "\n")}
+      />
+    </div>
+  );
+};
 
 export default ListFormatControls;
